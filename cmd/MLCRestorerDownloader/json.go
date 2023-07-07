48a8830200d604dae7b1cf5cf617e26617fe78e4
Xpl0itU <24777100+Xpl0itU@users.noreply.github.com>
2023-07-07 17:32:08 +0200

    Remove comments

--- cmd/MLCRestorerDownloader/json.go
+++ cmd/MLCRestorerDownloader/json.go
@@ -13,23 +13,19 @@ type TitleMap struct {
 }
 
 func readTitleInfoFromFile(filename string) (TitleMap, error) {
-	// Open the file
 	file, err := os.Open(filename)
 	if err != nil {
 		return TitleMap{}, fmt.Errorf("error opening file: %w", err)
 	}
 	defer file.Close()
 
-	// Read the file contents
 	jsonData, err := io.ReadAll(file)
 	if err != nil {
 		return TitleMap{}, fmt.Errorf("error reading file: %w", err)
 	}
 
-	// Define a map to hold the parsed data
 	titles := TitleMap{}
 
-	// Parse the JSON into the titles map
 	err = json.Unmarshal(jsonData, &titles)
 	if err != nil {
 		return TitleMap{}, fmt.Errorf("error parsing JSON: %w", err)
